5bb7e2621f059aa5d67f8f1844fd0ebb2d447bf1
liveacid <danny@liveacid.com>
2022-06-26 15:35:34 -0600

    revert back

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-const MongoStore = require('connect-mongo');
-const session = require('express-session');
 const mongodb = require('./mongodb');
-const config = require('./config');
-
-session({
-    secret: config.session.secret,
-    maxAge: config.session.maxAge,
-    resave: config.session.resave,
-    saveUninitialized: config.session.saveUninitialized,
-    store: MongoStore.create({
-        collection: config.session.collection,
-        clientPromise: mongodb.client(),
-    }),
-    cookie: {
-        maxAge: config.session.maxAge,
-        sameSite: 'lax',
-        httpOnly: true,
-        secure: config.NODE_ENV === 'development',
-    },
-});
+const session = require('./session');
 
 module.exports = {
-    session: session
+    client: mongodb.client,
+    session: session.session
 };
